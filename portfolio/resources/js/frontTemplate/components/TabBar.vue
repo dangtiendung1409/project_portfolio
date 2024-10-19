d799fab799dc47229b61c99a08048cdfd5b52b6d
dungdang <dangdunghn1409@gmail.com>
2024-10-19 11:06:07 +0700

    update

--- portfolio/resources/js/frontTemplate/components/TabBar.vue
+++ portfolio/resources/js/frontTemplate/components/TabBar.vue
@@ -1,26 +1,14 @@
 <template>
     <div class="app-bar">
         <div class="app-bar-content">
-            <div
-                class="app-bar-item"
-                :class="{ active: activeItem === 'following', selected: activeItem === 'following' }"
-                @click="setActive('following')"
-            >
-                Following
+            <div class="app-bar-item" :class="{ active: activeItem === 'following', selected: activeItem === 'following' }"
+                @click="setActive('following')"> Following
             </div>
-            <div
-                class="app-bar-item"
-                :class="{ active: activeItem === 'forYou', selected: activeItem === 'forYou' }"
-                @click="setActive('forYou')"
-            >
-                For You
+            <div class="app-bar-item" :class="{ active: activeItem === 'forYou', selected: activeItem === 'forYou' }"
+                @click="setActive('forYou')"> For You
             </div>
-            <div
-                class="app-bar-item"
-                :class="{ active: activeItem === 'explore', selected: activeItem === 'explore' }"
-                @click="setActive('explore')"
-            >
-                Explore
+            <div class="app-bar-item" :class="{ active: activeItem === 'explore', selected: activeItem === 'explore' }"
+                @click="setActive('explore')"> Explore
             </div>
         </div>
     </div>
